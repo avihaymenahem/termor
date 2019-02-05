b48c9645b867d36119183d7af687f699216d6cbc
avihay <avihay@three-dev.com>
2019-02-05 10:53:51 +0200

    Fixed top bar styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import MainView from './views/MainView';
 
 class App extends Component {
-  render() {
-    return (
-      <MainView/>
-    );
-  }
+	render() {
+		return (
+			<MuiThemeProvider>
+				<MainView/>
+			</MuiThemeProvider>
+		);
+	}
 }
 
 export default App;
