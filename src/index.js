6cf9c1442fba66c4fdc7cc4579eb9c47f6f01c53
avihay <avihay@three-dev.com>
2019-02-05 10:12:51 +0200

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import '../node_modules/xterm/dist/xterm.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './style.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
